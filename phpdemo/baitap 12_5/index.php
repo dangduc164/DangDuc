27754dd2d840d5fbc924efa6e221dba8e9bf94f5
Nguyễn Đăng Đức <82128934+dangduc164@users.noreply.github.com>
2021-05-25 09:42:55 +0700

    Add files via upload

---
+++ phpdemo/baitap 12_5/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>bài 1</title>
+</head>
+<body>
+	<?php
+	    $n;
+	    for ($i=0; $i < n; $i++) { 
+	    	# code...
+	    	
+	    }
+	?>
+
+</body>
+</html>
